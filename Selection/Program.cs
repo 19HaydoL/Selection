f1f55df2b3deb13bd73491a8e6d1d65988383d4f
Lewis Haydon <19HaydoL@fulford.york.sch.uk>
2024-09-13 10:08:33 +0100

    added switch

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -1,6 +1,54 @@
 ﻿Console.Write("What day of the week is it? ");
 string Day = Console.ReadLine();
 
+
+switch (Day.Trim().ToLower())
+{ 
+
+    case "monday";
+        Console.WriteLine("");
+        Console.WriteLine(" >:(  not skibidi!!!");
+        break;
+
+    case "tuesday";
+        Console.WriteLine("");
+        Console.WriteLine("Atleast its not monday!!!");
+        break;
+
+    case "wednesday";
+        Console.WriteLine("");
+        Console.WriteLine("Half way through the school week!");
+        break;
+
+    case "thursday";
+        Console.WriteLine("");
+        Console.WriteLine("Its almost friday!!!1!!1!!11!");
+        break;
+
+    case "friday";
+        Console.WriteLine("");
+        Console.WriteLine("Yippie, thats so skibidi!!!!");
+        break;
+
+    case "saturday";
+        Console.WriteLine("");
+        Console.WriteLine("WEEKEND WOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!");
+        break;
+
+    case "sunday";
+        Console.WriteLine("");
+        Console.WriteLine("school tomorrow  :(");
+        break;
+
+     default:
+        Console.WriteLine("Thats not even a day!! >:(");
+        break;
+}
+
+
+
+
+
 if (Day.ToUpper().Trim() == "FRIDAY")
 {
     Console.WriteLine("");
